fbea4e34a4f796ce1f03e071195d28e0c80531de
shruti2522 <shruti.apc01@gmail.com>
2025-08-27 08:20:32 +0000

    change

--- components/Loader/Loader.tsx
+++ components/Loader/Loader.tsx
@@ -5,7 +5,6 @@ import { motion } from "framer-motion";
 export default function PageLoader() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 pointer-events-none">
-      {/* Animated loader */}
       <motion.div
         className="w-16 h-16 rounded-full border-4 border-t-blue-500 border-blue-300"
         animate={{ rotate: 360 }}
